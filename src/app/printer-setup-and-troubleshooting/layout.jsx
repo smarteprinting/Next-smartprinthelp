2d8936a973962214bf5910f428566474f9ef7747
Satyam kumar <satyamkumar2716@gmail.com>
2026-07-07 02:11:08 +0530

    updated

--- src/app/printer-setup-and-troubleshooting/layout.jsx
+++ src/app/printer-setup-and-troubleshooting/layout.jsx
@@ -1,5 +1,11 @@
 import ClientLayout from '@/components/printer-setup/ClientLayout';
 
+export const metadata = {
+  title: {
+    template: '%s',
+  },
+};
+
 export default function PrinterSetupLayout({ children }) {
   return <ClientLayout>{children}</ClientLayout>;
 }
